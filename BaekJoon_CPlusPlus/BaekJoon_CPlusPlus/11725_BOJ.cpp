55b2a04fb8d21a787240482bb8b1645b5dc83e81
unsik6 <tlsguswns119@naver.com>
2022-03-03 13:09:47 +0900

    Q11725

---
+++ BaekJoon_CPlusPlus/BaekJoon_CPlusPlus/11725_BOJ.cpp
@@ -0,0 +1,48 @@
+// 백준 11725번 트리의 부모 찾기
+
+// input: 노드의 개수 (2<= N <= 100,000)
+//		  연결된 정점
+// 
+// condition: 각 트리의 부모를 출력
+
+// DFS using Matrix : Memory limit
+// BFS using Matrix : Memory limit
+
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector<int>* adjacent = new vector<int>[N + 1];
+	for (int i = 0; i < N - 1; i++)
+	{
+		int str, dst;
+		cin >> str >> dst;
+		adjacent[str].push_back(dst);
+		adjacent[dst].push_back(str);
+	}
+
+	int* parArr = new int[N + 1]{ 0 };
+	// DFS
+	stack<int> explored;
+	explored.push(1);
+	while (!explored.empty())
+	{
+		int cur = explored.top();
+		explored.pop();
+		for (int e : adjacent[cur])
+		{
+			if (parArr[e] == 0)
+			{
+				parArr[e] = cur;
+				explored.push(e);
+			}
+		}
+	}
+
+	for (int i = 2; i < N + 1; i++) cout << parArr[i] << '\n';
+}
