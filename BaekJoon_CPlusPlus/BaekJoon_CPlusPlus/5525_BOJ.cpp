0de2172ff39c20986951c1af4cd56f4fe82575e5
unsik6 <tlsguswns119@naver.com>
2022-05-16 18:47:05 +0900

    Q5525 solve

--- BaekJoon_CPlusPlus/BaekJoon_CPlusPlus/5525_BOJ.cpp
+++ BaekJoon_CPlusPlus/BaekJoon_CPlusPlus/5525_BOJ.cpp
@@ -25,104 +25,179 @@
 #include <iostream>
 using namespace std;
 
-#define M 1234567891
-
-void hashTableOfStr(char* _str, int _size, int _n, int** _hashTable)
-{
-	int* table = new int[_size - (2 * _n + 1) + 1];
-	int sumValue = 0;
-	for (int i = 0; i < (2 * _n + 1); i++)
-	{
-		sumValue += _str[i];
-	}
-	table[0] = sumValue % M;
-	for (int i = 1; i < _size - (2 * _n + 1) + 1; i++)
-	{
-		sumValue = sumValue - _str[i - 1] + _str[i + (2 * _n)];
-		table[i] = sumValue % M;
-	}
-	*_hashTable = table;
-}
-
-bool naiveStringMatching(char* _str1, int _startPosOfStr1, char* _str2, int _size)
-{
-	bool matching = true;
-	for (int i = 0; i < _size; i++)
-	{
-		if (_str1[_startPosOfStr1 + i] != _str2[i])
-		{
-			matching = false;
-			break;
-		}
-	}
-
-	return matching;
-}
-
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
 	int n, m;
 	cin >> n >> m;
 	char* str = new char[m + 1];
-	char* pattern = new char[2 * n + 1];
-	for (int i = 0; i < 2 * n + 1; i++)
-	{
-		if (i % 2 == 0)
-			pattern[i] = 'I';
-		else
-			pattern[i] = 'O';
-	}
-	/*cout << "pattern: ";
-	for (int i = 0; i < 2 * n + 1; i++)
-	{
-		cout << pattern[i];
-	}
-	cout << endl;*/
 	cin.ignore();
 	cin.clear();
 	cin.getline(str, m + 1);
 
-
-
-	if (2 * n + 1 == m)
+	int cnt = 0;
+	bool onI = false;
+	bool OI = true;
+	bool onMatching = false;
+	int numberOfO = 0;
+	for (int i = 0; i < m; i++)
 	{
-		if (naiveStringMatching(str, 0, pattern, 2 * n + 1))
+		if (str[i] == 'I')
 		{
-			cout << 1;
-		}
-		else cout << 0;
-	}
-	else
-	{
-		int cnt = 0;
-		int* hashTable = nullptr;
-		hashTableOfStr(str, m, n, &hashTable);
-		int hashValueOfP = 0;
-		for (int i = 0; i < 2 * n + 1; i++)
-		{
-			hashValueOfP += pattern[i];
-		}
-		hashValueOfP %= M;
-
-		for (int i = 0; i < m - (2 * n + 1) + 1; i++)
-		{
-			if (hashTable[i] == hashValueOfP)
+			if (!onI)
 			{
-				if (str[i] == 'I' && str[i + 1] == 'O' && str[i + 2] == 'I')
+				onI = true;
+			}
+			else
+			{
+				if (!OI)
 				{
-					if (naiveStringMatching(str, i, pattern, 2 * n + 1))
+					if (onMatching)
 					{
 						cnt++;
 					}
+					else
+					{
+						numberOfO++;
+						if (numberOfO == n)
+						{
+							cnt++;
+							onMatching = true;
+						}
+					}
+					OI = true;
+				}
+				else
+				{
+					onI = true;
+					OI = true;
+					onMatching = false;
+					numberOfO = 0;
 				}
 			}
 		}
-
-		cout << cnt;
-
-		delete[] hashTable;
+		else
+		{
+			if (onI)
+			{
+				if (!OI)
+				{
+					onI = false;
+					OI = true;
+					onMatching = false;
+					numberOfO = 0;
+				}
+				else OI = false;
+			}
+		}
 	}
+
+	cout << cnt;
+
+	delete[] str;
 }
+
+// 50점 짜리 라빈 카프
+//#include <iostream>
+//using namespace std;
+//
+//#define M 1234567891
+//
+//void hashTableOfStr(char* _str, int _size, int _n, int** _hashTable)
+//{
+//	int* table = new int[_size - (2 * _n + 1) + 1];
+//	int sumValue = 0;
+//	for (int i = 0; i < (2 * _n + 1); i++)
+//	{
+//		sumValue += _str[i];
+//	}
+//	table[0] = sumValue % M;
+//	for (int i = 1; i < _size - (2 * _n + 1) + 1; i++)
+//	{
+//		sumValue = sumValue - _str[i - 1] + _str[i + (2 * _n)];
+//		table[i] = sumValue % M;
+//	}
+//	*_hashTable = table;
+//}
+//
+//bool naiveStringMatching(char* _str1, int _startPosOfStr1, char* _str2, int _size)
+//{
+//	bool matching = true;
+//	for (int i = 0; i < _size; i++)
+//	{
+//		if (_str1[_startPosOfStr1 + i] != _str2[i])
+//		{
+//			matching = false;
+//			break;
+//		}
+//	}
+//
+//	return matching;
+//}
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//	int n, m;
+//	cin >> n >> m;
+//	char* str = new char[m + 1];
+//	char* pattern = new char[2 * n + 1];
+//	for (int i = 0; i < 2 * n + 1; i++)
+//	{
+//		if (i % 2 == 0)
+//			pattern[i] = 'I';
+//		else
+//			pattern[i] = 'O';
+//	}
+//	/*cout << "pattern: ";
+//	for (int i = 0; i < 2 * n + 1; i++)
+//	{
+//		cout << pattern[i];
+//	}
+//	cout << endl;*/
+//	cin.ignore();
+//	cin.clear();
+//	cin.getline(str, m + 1);
+//
+//
+//
+//	if (2 * n + 1 == m)
+//	{
+//		if (naiveStringMatching(str, 0, pattern, 2 * n + 1))
+//		{
+//			cout << 1;
+//		}
+//		else cout << 0;
+//	}
+//	else
+//	{
+//		int cnt = 0;
+//		int* hashTable = nullptr;
+//		hashTableOfStr(str, m, n, &hashTable);
+//		int hashValueOfP = 0;
+//		for (int i = 0; i < 2 * n + 1; i++)
+//		{
+//			hashValueOfP += pattern[i];
+//		}
+//		hashValueOfP %= M;
+//
+//		for (int i = 0; i < m - (2 * n + 1) + 1; i++)
+//		{
+//			if (hashTable[i] == hashValueOfP)
+//			{
+//				if (str[i] == 'I' && str[i + 1] == 'O' && str[i + 2] == 'I')
+//				{
+//					if (naiveStringMatching(str, i, pattern, 2 * n + 1))
+//					{
+//						cnt++;
+//					}
+//				}
+//			}
+//		}
+//
+//		cout << cnt;
+//
+//		delete[] hashTable;
+//	}
+//}
